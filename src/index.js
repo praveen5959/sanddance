011cb48ffcb29cd6769f900d546aa89e3c7cffb3
praveen5959 <praveenbhat98@gmail.com>
2021-11-03 19:41:09 +0530

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import "@msrvida/sanddance-explorer/dist/css/sanddance-explorer.css";
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as deck from '@deck.gl/core';
+import * as layers from '@deck.gl/layers';
+import * as luma from '@luma.gl/core';
+import * as fluentui from '@fluentui/react';
+import * as vega from 'vega';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { Explorer, use } from '@msrvida/sanddance-explorer';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+fluentui.initializeIcons();
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+use(fluentui, React, ReactDOM, vega, deck, layers, luma);
+
+const data = [
+  { a: 1, b: "c1" },
+  { a: 1, b: "c2" },
+  { a: 2, b: "c3" },
+  { a: 3, b: "c4" }
+];
+
+const explorerProps = {
+    logoClickUrl: 'https://microsoft.github.io/SandDance/',
+    mounted: explorer => {
+        explorer.load(data);
+    }
+};
+
+ReactDOM.render(React.createElement(Explorer, explorerProps), document.getElementById('root'));
